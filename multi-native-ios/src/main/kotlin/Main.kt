54598c74e8b16ad3fef7ca1e1f37387ca3dc8002
Colton Nohelty <noheltycolton@gmail.com>
2018-08-08 23:51:07 -0400

    Stubbed i386 architecture

---
+++ multi-native-ios/src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+class KotlinIOS {
+  fun accessCommon() {
+  }
+}
--- prepare-cocoapod-framework.sh
+++ prepare-cocoapod-framework.sh
@@ -1,4 +1,28 @@
-./gradlew compileKonanMultiNative
+./gradlew build
+
+if ! [ -x "$(command -v xcodebuild)" ]; then
+  echo 'Error: xcodebuild is not installed.' >&2
+  exit 1
+fi
+
+xcodebuild \
+  -project multi-native-ios/MultiNativei386Stub/MultiNativei386Stub.xcodeproj \
+  -scheme MultiNativei386Stub \
+  -configuration Release \
+  -sdk iphoneos CONFIGURATION_BUILD_DIR=./build clean build
+
+if ! [ -x "$(command -v lipo)" ]; then
+  echo 'Error: lipo is not installed.' >&2
+  exit 1
+fi
+
+lipo -create \
+  -arch arm64 multi-native-ios/build/konan/bin/ios_arm64/MultiNative.framework/MultiNative \
+  -arch x86_64 multi-native-ios/build/konan/bin/ios_x64/MultiNative.framework/MultiNative \
+  -arch i386 multi-native-ios/MultiNativei386Stub/build/MultiNativei386Stub.framework/MultiNativei386Stub \
+  -output build/ios/release/MultiNative.framework/MultiNative
+
+zip -r build/MultiNative.zip LICENSE build/ios/release
 
 if ! [ -x "$(command -v pod)" ]; then
   echo 'Error: Coacoapods is not installed.' >&2
